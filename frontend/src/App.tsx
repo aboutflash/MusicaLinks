c5abdbf8e5f6297cf2feb3cd5eaade728b2c1cf7
Falk von Zydowitz <falk@aboutflash.de>
2023-11-27 16:12:35 +0100

    feat: added API generation for Tidal

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+function App() {
+  return (
+    <Centered>APP</Centered>
+  )
+}
+
+export default App
+
+const Centered = styled.div`
+  margin: 0 auto;
+  text-align: center;
+`
